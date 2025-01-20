6fba95e9bde9e29d68ca7549c9d27654367bf5ab
Luca Francis <luca@MacBook-Air-von-Luca.local>
2025-01-20 13:40:53 +0100

    use 80B model for gsm8k lexicon

--- converter/converter.py
+++ converter/converter.py
@@ -1,23 +1,29 @@
 import ast
+from concurrent.futures import ThreadPoolExecutor
 
 from datasets import Dataset
-from concurrent.futures import ThreadPoolExecutor
+
 
 def upload(dataset_dict, repo_id):
     for subset in dataset_dict.keys():
         print("Uploading", subset)
         dataset_dict[subset].push_to_hub(repo_id, subset)
 
+
 standard_subject = "Sebastian"
+
+
 def add_sentence_to_question(question: str, subject: str) -> str:
     return question + " " + f"{subject} goes to buy icecream."
+
 
 def check_if_dataset_contains_naive_sentence(df, question_column: str, use_custom_subjects: bool):
     if not use_custom_subjects:
         print(f"Rows mentioning {standard_subject}:", len(df[df[question_column].str.contains(standard_subject)]))
     print("Rows mentioning 'icecream':", len(df[df[question_column].str.contains("icecream")]))
 
-def convert_naive_row(question_column, subject_list_column = None):
+
+def convert_naive_row(question_column, subject_list_column=None):
     def row_function(row):
         if subject_list_column is not None:
             subject: str = ast.literal_eval(row[subject_list_column])[0]
@@ -25,54 +31,62 @@ def convert_naive_row(question_column, subject_list_column = None):
         else:
             row[question_column] = add_sentence_to_question(row[question_column], standard_subject)
         return row
+
     return row_function
 
-def convert_naive(df, question_column: str = 'question', subject_list_column = None) -> Dataset:
+
+def convert_naive(df, question_column: str = 'question', subject_list_column=None) -> Dataset:
     check_if_dataset_contains_naive_sentence(df, question_column, subject_list_column is not None)
     df_naive = df.progress_apply(convert_naive_row(question_column), axis=1)
     return Dataset.from_pandas(df_naive)
 
+
 def add_additional_information_to_question(question: str, client, model: str, custom_preprompt) -> str:
-    standard_preprompt = "Give me one additional sentence to this question that has no direct effect on the question. The sentence should be about a similar topic. It should not be a question. Example additional sentences might focus on unnecessary details, add unnecessary information about other fields of study. Only output exactly the additional sentence and nothing else, the output will be copy/pasted as is. It is extremely important that the sentence does not effect the answer to the question."
+    standard_preprompt = ("""Output one additional sentence to this question that has no direct effect on the question.
+The sentence should be on topic, designed to confuse a inattentive reader. 
+Therefore it should not be a question and not change anything about the answer to the question.
+Example additional sentences might focus on specific unrelated details or add information about the topic which does not contribute to the problem solving.
+Example:
+Original Text:
+"Paul is downloading a 125 GB file.
+Normally he can download 2 GB/minute, but 40% of the way through the download, Windows forces a restart to install updates, which takes 20 minutes.
+Then Paul has to restart the download from the beginning.
+How load does it take to download the file?"
+
+Additional Sentence added:
+"The Windows update adds new features that resolve issues for unstable internet connections."
+
+Explanation (Do not add this in your output when generating the additional sentence):
+The additional features added to not change the answer to the question as Paul is not mentioned to have a unstable connection.
+
+Only output exactly the additional sentence and nothing else, the output will be copy/pasted as is. 
+It is extremely important that the sentence does not effect the answer to the question.""")
     preprompt = custom_preprompt if custom_preprompt is not None else standard_preprompt
     addition_prompt = lambda q: str(preprompt + f"\nQuestion: \n'{q}'\nAdditional sentence:")
-    response = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": addition_prompt(question),
-            }
-        ],
-        model=model,
-    )
+    response = client.chat.completions.create(messages=[{"role": "user", "content": addition_prompt(question), }],
+        model=model, )
     result = response.choices[0].message.content
     return question + " " + result
 
+
 def convert_additional_row(question_column: str, client, model: str, custom_preprompt):
     def row_function(row):
-        row[question_column] = add_additional_information_to_question(row[question_column], client, model, custom_preprompt)
+        row[question_column] = add_additional_information_to_question(row[question_column], client, model,
+                                                                      custom_preprompt)
         return row
+
     return row_function
+
 
 def convert_additional(df, client, model, question_column: str = 'question', custom_preprompt=None) -> Dataset:
     df_additional = df.progress_apply(convert_additional_row(question_column, client, model, custom_preprompt), axis=1)
     return Dataset.from_pandas(df_additional)
 
+
 def fetch_alternative_word(blanked, original_word, client, model):
     prompt = f"Output exactly one word that fits where the placeholder '<BLANK>' is placed and has similar meaning to '{original_word}'. No other output besides the word.\nText: '{''.join(blanked)}'"
-    response = client.chat.completions.create(
-        model=model,
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
-        ],
-        max_tokens=5,
-        top_logprobs=5,
-        logprobs=True,
-        temperature=0.0,
-    )
+    response = client.chat.completions.create(model=model, messages=[{"role": "user", "content": prompt, }],
+        max_tokens=5, top_logprobs=5, logprobs=True, temperature=0.0, )
     choice = response.choices[0]
     if choice.logprobs.content[0].logprob >= -1:
         return choice.message.content.lower().strip().replace(".", "")
@@ -100,15 +114,19 @@ def paraphrase_question(question, nlp, client, model):
 
     return "".join(words)
 
+
 def convert_lexicon_row(question_column, client, model, nlp):
     def row_function(row):
         row[question_column] = paraphrase_question(row[question_column], nlp, client, model)
         return row
+
     return row_function
 
+
 def convert_lexicon(df, client, model, nlp, question_column='question'):
-    df_lex = df.progress_apply(convert_lexicon_row(question_column,client, model, nlp), axis=1)
+    df_lex = df.progress_apply(convert_lexicon_row(question_column, client, model, nlp), axis=1)
     return Dataset.from_pandas(df_lex)
+
 
 def rephrase_question(question, nlp):
     # Parse the sentence
@@ -134,17 +152,21 @@ def rephrase_question(question, nlp):
 
     return reordered
 
+
 def split_question_into_sentences(question, nlp):
     doc = nlp(question)
     sentences = [sent.text for sent in doc.sents]
     return sentences
+
 
 def convert_syntax_row(question_column, nlp):
     def row_function(row):
         row[question_column] = "".join(
             [rephrase_question(sentence, nlp) for sentence in split_question_into_sentences(row[question_column], nlp)])
         return row
+
     return row_function
+
 
 def convert_syntax(df, nlp, question_column="question"):
     df_syn = df.progress_apply(convert_syntax_row(question_column, nlp), axis=1)
